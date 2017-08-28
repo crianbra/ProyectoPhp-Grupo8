a7364b761ac684b8c15a6affa6b1b9c0df658d2a
k <alexaorte1989@gmail.com>
2017-08-27 23:11:09 -0500

    creando clase alumnoxMateria

---
+++ php/alumnoxmateria/AlumnoxMateria.php
@@ -0,0 +1,35 @@
+<?php
+class AlumnoxMateria
+{
+    private $idalumno;
+    private $descripcion;
+    private $usuarioid;
+    
+     function __construct($idalumno, $descripcion, $usuarioid) {
+       	$this->idalumno = $idalumno;
+       	$this->descripcion = $descripcion;
+        $this->usuarioid = $usuarioid;
+      
+     }
+    
+     function setIdAlumno($idalumno){
+       $this->idalumno = $idalumno;
+     } 
+     function getIdAlumno(){
+       return $this->idalumno;
+     } 
+     function setDescripcion($descripcion){
+       $this->descripcion = $descripcion;
+     } 
+     function getDescripcion(){
+       return $this->descripcion;
+     }
+     function setUsuarioid($usuarioid){
+       $this->usuarioid = $usuarioid;
+     } 
+     function getUsuarioid(){
+       return $this->usuarioid;
+     }
+     
+}
+?> 
