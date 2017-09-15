792b091ff0e5df663cd2aae021376617fc3f1b25
alexaortepe <alexaortepe1989@gmail.com>
2017-09-14 22:53:49 -0500

    editando

--- php/bd/Collector.php
+++ php/bd/Collector.php
@@ -2,10 +2,10 @@
 include_once('dataBase.php');
 
 // Define configuration
-define("DB_HOST", "localhost");
-define("DB_USER", "postgres");
-define("DB_PASS", "1234");
-define("DB_NAME", "educaciononline2");
+define("DB_HOST", "ec2-54-225-88-199.compute-1.amazonaws.com");
+define("DB_USER", "uzxaqhjmjxncxa");
+define("DB_PASS", "d0747a93621a7131e779199b9ab67e5457476bf8a081eb9b897d473d5b950e74");
+define("DB_NAME", "d6pba8juduci4s");
 
 
 class Collector extends dataBase
