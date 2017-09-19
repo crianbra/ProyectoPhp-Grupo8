289849f99a4be19d7d8dada21ae0af2420859c5c
crianbra <crianbra@espol.edu.ec>
2017-09-18 22:36:38 -0500

    Educapp

--- admin/usuario/validarUsuario.php
+++ admin/usuario/validarUsuario.php
@@ -14,9 +14,11 @@ require('validar.php');
         //$perfil = $_POST['perfil'];
         if ($usuarioCollector->validarUsuario($nombreusuario,$contrasenia)){
             $_SESSION['mySesion'] = $nombreusuario;
+            echo "<p> Hola usuario:(" . $_SESSION['mySesion']. ")
+            [<a href='../sitioweb/index.php'>Ingresar</a>]";
             //$_SESSION['rol'] = Usuario::$perfil;
             //$_SESSION['rol'] = Usuario::$perfil; //agrege una sesion rol para identificar al perfil del usuario
-            echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=../sitioweb/index.php'>";
+            //echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=../sitioweb/index.php'>";
         }
         else{
              $mensaje = "EL USUARIO NO SE ENCUENTRA REGISTRADO";
